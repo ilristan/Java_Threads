39d23bfe66b298cf4f65e81b940437e747e0a725
ilristan <ilristan@ucalgary.ca>
2017-03-22 18:15:07 -0600

    Initial commit

---
+++ src/Processor.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ivan on 3/21/2017.
+ */
+public class Processor {
+}
---
+++ src/TokenRing.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ivan on 3/21/2017.
+ */
+public class TokenRing {
+
+    public int loac(int x)
+    {
+        return 0;
+    }
+
+    public int store()
+
+}
