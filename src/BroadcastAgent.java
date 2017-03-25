2d1497f6a8271cae16ccf1fcc20f868c350c5cf0
ilristan <ilristan@ucalgary.ca>
2017-03-25 13:49:56 -0600

    Questions 1 & 2 done with commenting

--- src/BroadcastAgent.java
+++ src/BroadcastAgent.java
@@ -3,26 +3,33 @@
  */
 public class BroadcastAgent {
 
-
+    // initializes variables
     private BroadcastSystem system;
+    private LocalMemory localMem;
 
-    public BroadcastAgent(BroadcastSystem system)
+    //constructor
+    public BroadcastAgent(BroadcastSystem system, LocalMemory localMem)
     {
-        this.system = system;
+        this.system = system; //sets the reference to the broadcast system
+        this.localMem = localMem; //sets the reference to the local memory
     }
+
+    // method to send messages to the broadcast system
     public void Broadcast(String message)
     {
-        system.enqueue(message);
+        system.enqueue(message); // adds the message to the broadcast systems queue
     }
 
-    public void receive()
+    // mehtod to receive stores from the broadcast system
+    public void receive(String x, int v)
     {
-
+        localMem.store(x, v); // stores the message from the broadcast system to its localMem
     }
 
+    // getter for the broadcast system
     public BroadcastSystem getSystem()
     {
-        return this.system;
+        return this.system; // returns the system
     }
 
 
--- src/Processor.java
+++ src/Processor.java
@@ -3,29 +3,37 @@
  */
 public class Processor extends Thread{
 
+    // initializing variables
     private BroadcastSystem system;
     private DSM dsm;
     private int pid;
     private boolean f = false;
+
+    // constructor
     Processor(BroadcastSystem system, int pid)
     {
         this.system = system;
         this.pid = pid;
-        dsm = new DSM(system);
+        dsm = new DSM(system);  // creates a DSM
 
     }
 
+    // implementing peterson's alogrithm
     public void run()
     {
+        // initializes the flag values to -1 to prevent null pointer errors in the for loop (2 down)
         for(int i = 0; i <10 ; i++)
         {
             dsm.store("flag"+Integer.toString(i),-1);
         }
+
+        // intializes turn values to -1 to prevent null pointer errors in the for loop below
         for(int i = 0; i <9 ; i++)
         {
             dsm.store("turn"+Integer.toString(i),-1);
         }
 
+        // peterson's algorithm
         for (int floor = 0; floor < 9; floor++) {
             dsm.store("flag"+pid,floor); //flag[processID] = floor;
             dsm.store("turn"+floor, pid);  //turn[floor] = processID;
@@ -45,15 +53,22 @@ public class Processor extends Thread{
         }
 
         System.out.println("im in the critical section " + pid);
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         dsm.store("flag"+pid,-1);  //flag[processID] = -1;
         System.out.println("im exiting the critical section " + pid);
     }
 
+    // getter for the process ID
     public int getPid()
     {
         return this.pid;
     }
 
+    // getter for the DSM
     public DSM getDsm()
     {
         return this.dsm;
