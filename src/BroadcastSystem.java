2e2f15d15d31c5b912c47f55bfed4140b1df99ce
ilristan <ilristan@ucalgary.ca>
2017-03-24 18:54:29 -0600

    test commit

--- src/BroadcastSystem.java
+++ src/BroadcastSystem.java
@@ -6,8 +6,13 @@ import java.util.LinkedList;
 /**
  * Created by ivan on 3/21/2017.
  */
-
 public class BroadcastSystem {
+
+//    Queue<String> BSQueue = new LinkedList<String>() {
+//
+//    }
+
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
-/**
- * Created by ivan on 3/22/2017.
- */
+
+
 public class Main {
 
     public static void main (String[] args)
     {
-        BroadcastSystem BS = new BroadcastSystem();
+        ProcessorQueue queue  = new ProcessorQueue();
+
+        queue.enqueue("Hello 9");
+        String currentProcessor = queue.dequeue();
+        String[] turn = currentProcessor.split(" ");
+
+        System.out.println(turn[1]);
 
 
 
-        Processor p1 = new Processor();
-        Processor p2 = new Processor();
-        Processor p3 = new Processor();
-        Processor p4 = new Processor();
-        Processor p5 = new Processor();
-        Processor p6 = new Processor();
-        Processor p7 = new Processor();
-        Processor p8 = new Processor();
-        Processor p9 = new Processor();
-        Processor p10 = new Processor();
-
-        p1.start();
-        p2.start();
-        p3.start();
-        p4.start();
-        p5.start();
-        p6.start();
-        p7.start();
-        p8.start();
-        p9.start();
-        p10.start();
+        //Processor process1 = new Processor();
     }
 }
